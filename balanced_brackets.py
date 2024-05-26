ef26c4da6022ba92dfc5458d2464c10f866fdb87
zlatevv <146636754+zlatevv@users.noreply.github.com>
2024-05-26 12:12:01 +0300

    Update for  balanced_brackets.py

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -1,21 +1,21 @@
-number_of_lines = int(input()) 
+number_of_lines = int(input())
 balanced = True
-open_bracket = True
-for _ in range(number_of_lines):
-    text = input()
-    if text == "(":
-        if open_bracket:
+open_bracked = False
+for i in range(number_of_lines):
+    line = input()
+    if line == "(":
+        if open_bracked:
             balanced = False
-            break
-        open_bracket = True
-    elif text == ")":
-        if not open_bracket:
+        else:
+            open_bracked = True
+    elif line == ")":
+        if not open_bracked:
             balanced = False
-            break
-        open_bracket = False
-if open_bracket:
+        else:
+            open_bracked = False
+if open_bracked:
     balanced = False
-if balanced:       
+if balanced:
     print("BALANCED")
 else:
     print("UNBALANCED")
